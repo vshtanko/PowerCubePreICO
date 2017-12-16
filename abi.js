b2b7acd1189fb375833d0706983ddceac85e45df
Штанько Владимир <shtankova@gmail.com>
2017-12-16 13:03:53 +0300

    Release 1.0

---
+++ abi.js
@@ -0,0 +1,230 @@
+[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "balance",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "currentSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newAdmin",
+				"type": "address"
+			}
+		],
+		"name": "changeAdmin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "burn",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getPreSaleTokenPool",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "admin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Mint",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Burn",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [],
+		"name": "MintFinished",
+		"type": "event"
+	}
+]
--- contracts/preICO.sol
+++ contracts/preICO.sol
@@ -1,31 +1,75 @@
 pragma solidity ^0.4.18;
 
-contract PowerCubePreIcoToken {
+contract PowerCubePreSale {
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 
     mapping(address => uint256) balances;
+    address public admin;
     uint256 public totalSupply;
+    uint256 public currentSupply;
     event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed from, uint256 amount);
     event MintFinished();
 
-    function PowerCubePreIcoToken() {
-    
+    string public constant symbol = "PCB/1";
+    string public constant name = "PowerCube preSale";
+    uint8 public constant decimals = 0;
+
+    function PowerCubePreSale() public {
+        admin = msg.sender;
+        totalSupply = 10000;
+        currentSupply = totalSupply;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
-        totalSupply = totalSupply.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
+    function totalSupply() public view returns (uint) {
+        return totalSupply;
+    }
+
+    function currentSupply() public view returns (uint) {
+        return currentSupply;
+    }
+
+    function getPreSaleTokenPool() public view returns (uint) {
+        return totalSupply - currentSupply;
+    }
+
+    function mint(address _to, uint256 _amount) onlyAdmin public returns (bool) {
+        require(_amount <= currentSupply);
+        currentSupply = sub(currentSupply, _amount);
+        balances[_to] = add(balances[_to], _amount);
         Mint(_to, _amount);
-        Transfer(address(0), _to, _amount);
         return true;
     }
 
-    function finishMinting() onlyOwner canMint public returns (bool) {
-        mintingFinished = true;
-        MintFinished();
+    function burn(address _from, uint256 _amount) onlyAdmin public returns (bool) {
+        require(_amount <= balances[_from]);
+        currentSupply = add(currentSupply, _amount);
+        balances[_from] = sub(balances[_from], _amount);
+        Burn(_from, _amount);
+        return true;
+    }
+
+    function changeAdmin(address newAdmin) onlyAdmin public returns (bool) {
+        admin = newAdmin;
         return true;
     }
 
